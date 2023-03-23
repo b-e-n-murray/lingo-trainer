7d662cefe353d9998026fbdb16580c806bbb399b
Ben Murray <benmurray573@gmail.com>
2023-03-23 15:53:43 +0000

    successfully fetching 4 and 5 letter words from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { greet } from "./utils/greet";
+import axios from "axios";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  async function fetchAndStoreWord(wordLength: number) {
+    console.log(wordLength);
+    const fetchedWord = await axios.get(
+      `https://random-word-api.herokuapp.com/word?length=${wordLength}`
+    );
+    console.log(fetchedWord);
+  }
+  return (
+    <>
+      <h1>LINGO</h1>
+      <button onClick={() => fetchAndStoreWord(4)}>Get 4 letter word</button>
+      <button onClick={() => fetchAndStoreWord(5)}>Get 5 letter word</button>
+    </>
+  );
 }
 
 export default App;
