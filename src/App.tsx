991d3d62cc8e8e84fc260d75298cdbf5721ebc5e
Ben Murray <benmurray573@gmail.com>
2023-03-24 16:26:11 +0000

    added useeffect and createWordStatusObj to track player's guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App(): JSX.Element {
+  const [currentWord, setCurrentWord] = useState<string | undefined>(undefined);
+  const [guessStatus, setGuessStatus] = useState<Record<string, string>>({});
+
   async function fetchAndStoreWord(wordLength: number) {
-    console.log(wordLength);
-    const fetchedWord = await axios.get(
+    const wordData = await axios.get(
       `https://random-word-api.herokuapp.com/word?length=${wordLength}`
     );
-    console.log(fetchedWord);
+    setCurrentWord(wordData.data);
   }
+
+  function createWordStatusObject(
+    currentWord: string | undefined
+  ): Record<string, string> {
+    const statusObj: Record<string, string> = {};
+    if (currentWord === undefined) {
+      console.log("current word is empty");
+    } else {
+      const splitWord: string[] = currentWord[0].split("");
+      for (const letter of splitWord) {
+        if (letter === splitWord[0]) {
+          statusObj[letter] = "correct";
+        } else {
+          statusObj[letter] = "incorrect";
+        }
+      }
+      return statusObj;
+    }
+    return {};
+  }
+
+  useEffect(() => {
+    setGuessStatus(createWordStatusObject(currentWord));
+  }, [currentWord]);
+
+  console.log(guessStatus);
   return (
     <>
       <h1>LINGO</h1>
       <button onClick={() => fetchAndStoreWord(4)}>Get 4 letter word</button>
       <button onClick={() => fetchAndStoreWord(5)}>Get 5 letter word</button>
+      <p>{currentWord}</p>
     </>
   );
 }
