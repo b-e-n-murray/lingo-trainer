6efd40f8cb825297b5b295deb02c1abb87a71f99
Ben Murray <benmurray573@gmail.com>
2023-03-24 16:54:58 +0000

    rendering fetched word with separated letters and ctn for each

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App(): JSX.Element {
   const [currentWord, setCurrentWord] = useState<string | undefined>(undefined);
-  const [guessStatus, setGuessStatus] = useState<Record<string, string>>({});
+  const [guessStatus, setGuessStatus] = useState<
+    Record<string, "correct" | "half-correct" | "incorrect">
+  >({});
 
   async function fetchAndStoreWord(wordLength: number) {
     const wordData = await axios.get(
@@ -14,8 +17,9 @@ function App(): JSX.Element {
 
   function createWordStatusObject(
     currentWord: string | undefined
-  ): Record<string, string> {
-    const statusObj: Record<string, string> = {};
+  ): Record<string, "correct" | "half-correct" | "incorrect"> {
+    const statusObj: Record<string, "correct" | "half-correct" | "incorrect"> =
+      {};
     if (currentWord === undefined) {
       console.log("current word is empty");
     } else {
@@ -42,7 +46,18 @@ function App(): JSX.Element {
       <h1>LINGO</h1>
       <button onClick={() => fetchAndStoreWord(4)}>Get 4 letter word</button>
       <button onClick={() => fetchAndStoreWord(5)}>Get 5 letter word</button>
-      <p>{currentWord}</p>
+      <div className="word-ctn">
+        {currentWord &&
+          currentWord[0].split("").map((letter) => {
+            return letter === currentWord[0][0] ? (
+              <div key={letter} className="letter-ctn">
+                {letter}
+              </div>
+            ) : (
+              <div key={letter} className="letter-ctn"></div>
+            );
+          })}
+      </div>
     </>
   );
 }
